4c271c3bd82623e50ccc731b547d1e69844aac6f
CowinUnderwood23 <817281@mystma.org>
2021-09-21 14:35:34 -0500

    2

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 def topFiveMovies(a, b, c, d, e):
     return a + b + c + d + e
 
-topFiveMovies(" shrek 1", " shrek 2", " shrek 3", " shrek 4", " shrek 5")
+print (topFiveMovies("shrek ", "shrek 2 ", "shrek 3 ", "shrek 4 ", "shrek 5 "))
